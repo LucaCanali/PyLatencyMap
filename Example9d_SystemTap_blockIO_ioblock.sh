377201a792db20ba5dfd7fad6b08f2d094fd61a3
Luca Canali <luca.canali@cern.ch>
2015-05-30 09:56:29 +0200

    version 1.2.1

---
+++ Example9d_SystemTap_blockIO_ioblock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is an example launcher script for PyLatencyMap 
+# Collect and display SystemTap data for the block I/O request calls as a Frequency-Intensity Latency HeatMap
+# This can be used to investigate single-block read latency 
+# Note this scripts requires to have SystemTap installed 
+
+stap -v SystemTap/blockio_ioblock_latency.stp |python SystemTap/systemtap_connector.py |python LatencyMap.py
+
