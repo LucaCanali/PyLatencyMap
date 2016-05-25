65227b960bc9b4f508b38cf4fb4ffc67918d8ca6
Luca Canali <luca.canali@cern.ch>
2016-05-25 15:56:40 +0200

    added BPF/bcc scripts

--- Example11_BPF-bcc_blockIO_latency.sh
+++ Example11_BPF-bcc_blockIO_latency.sh
@@ -3,7 +3,8 @@
 # This is an example launcher script for PyLatencyMap
 # Collect and display block I/O latency data using BPF-bcc/pylatencymap-biolatency.py
 # This can be used to investigate block I/O latency
-# Note this scripts requires to have BPF/bcc installed (Linux kernel >= 4.1)
+# Note this scripts requires BPF/bcc 
+# Tested on: Fedora 24 (beta) using kernel v. 4.6
 
 stdbuf -oL python BPF-bcc/pylatencymap-biolatency.py -QT 3 100 | python LatencyMap.py --min_bucket=4
 
